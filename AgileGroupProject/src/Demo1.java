be5c34e85834ed590a90a156186f02accc600089
Kieran Daly <Kieran Daly@DESKTOP-F80GNQB.home>
2026-02-15 15:18:35 +0000

    Engineer C: add method3

--- AgileGroupProject/src/Demo1.java
+++ AgileGroupProject/src/Demo1.java
@@ -1,21 +1,27 @@
 public class Demo1 {
-	
+
 	// method1 calculates and returns the square of a number
 	public float method1(float number) {
-		
+
 		float square = number * number;
 		return square;
-		
-	} // end method
-	
-	 // method2 checks a character and number and returns a boolean result
+
+	} // end method1
+
+	// method2 checks a character and number and returns a boolean result
 	public boolean method2(char letter, float number) {
 
 		if (letter == 'A' && number == 12.0) {
-		    return false;
+			return false;
 		}
 
 		return true;
 
 	} // end method2
-}
+
+	public char method3() {
+
+		return 'Z';
+
+	} // end method3
+} // end Demo1
