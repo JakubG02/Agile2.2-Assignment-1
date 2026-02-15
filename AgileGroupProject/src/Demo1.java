a64cc425386e5705f347636427ec77cf1a49631d
Kieran Daly <Kieran Daly@DESKTOP-F80GNQB.home>
2026-02-15 11:30:36 +0000

    Added comments to Demo1 methods

--- AgileGroupProject/src/Demo1.java
+++ AgileGroupProject/src/Demo1.java
@@ -1,15 +1,19 @@
 public class Demo1 {
 	
+	// method1 calculates and returns the square of a number
 	public float method1(float number) {
 		
 		float square = number * number;
 		return square;
 		
 	} // end method
+	
+	 // method2 checks a character and number and returns a boolean result
 	public boolean method2(char letter, float number) {
 
-		if ((letter == 'A') && (number == 12.0))
-				return false;
+		if (letter == 'A' && number == 12.0) {
+		    return false;
+		}
 
 		return true;
 
